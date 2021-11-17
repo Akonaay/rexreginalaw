8e7821c1bec1d27dbecc1772744928e8fc7a5bc7
akonnay <ambrosesulley4@gmail.com>
2021-11-17 15:55:31 +0300

    UI

---
+++ precache-manifest.7f651857e76eb8967442e1c90f4b2133.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542debeaf5b104e15b3d9b0302836b80",
+    "url": "/css/style-starter.css"
+  },
+  {
+    "revision": "dcd45129753b765f39aa4c5547088a12",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "0d2717cd5d853e5c765ca032dfd41a4d",
+    "url": "/fonts/FontAwesome.otf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fonts/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "b9328a37e4f5aade43e4d1df08b8d421",
+    "url": "/images/3619-2.jpg"
+  },
+  {
+    "revision": "1946b335f0b1b1b7dc2165890065dd1d",
+    "url": "/images/3656-2.jpg"
+  },
+  {
+    "revision": "c3637e4690f98e7a8660cf48045109d8",
+    "url": "/images/about-firm.jpg"
+  },
+  {
+    "revision": "09a48e27483ee2d4703ea52408ad7a4a",
+    "url": "/images/award.png"
+  },
+  {
+    "revision": "259fb31bcceaef328382eeb6ad543257",
+    "url": "/images/banners/banner1.jpg"
+  },
+  {
+    "revision": "64e7c206dd7172a0687ad931e1217bd6",
+    "url": "/images/banners/banner2.jpg"
+  },
+  {
+    "revision": "98695648c90cc1236e3b15fbaa05036a",
+    "url": "/images/banners/banner3.jpg"
+  },
+  {
+    "revision": "bb61c7747e9313b0340c7640ecb0155c",
+    "url": "/images/contact-bg.jpg"
+  },
+  {
+    "revision": "e58fc4f60b898080d7d2824d0740157d",
+    "url": "/images/contact-bg1.jpg"
+  },
+  {
+    "revision": "0488a41d68aa36816931cac2be18bec3",
+    "url": "/images/diego-jimenez-A-NVHPka9Rk-unsplash.jpg"
+  },
+  {
+    "revision": "05236bbf772bf78ae0571de83905a913",
+    "url": "/images/firm-2.jpg"
+  },
+  {
+    "revision": "8cc76c4b869121c65f3ceb048057062c",
+    "url": "/images/firm-bg.jpg"
+  },
+  {
+    "revision": "97c56f247ff8f3204add0d3dd4d192c0",
+    "url": "/images/firm-bg1.jpg"
+  },
+  {
+    "revision": "dae657978e27fd7a08bbd5f76296688b",
+    "url": "/images/joshua-earle-g1TWbj5XYb4-unsplash.jpg"
+  },
+  {
+    "revision": "5f4a503afcc43f62302ca7fed09dd7d0",
+    "url": "/images/logo.svg"
+  },
+  {
+    "revision": "8f142c5284c92316f21169ffce43fa4c",
+    "url": "/images/mission.jpg"
+  },
+  {
+    "revision": "76b2f88bd7812a9d423f44ae3cc324a2",
+    "url": "/images/patients.png"
+  },
+  {
+    "revision": "7a4db6df63ae1dbeeebc37d9a35bfbc0",
+    "url": "/images/photo_2020-02-24_10-14-28.jpg"
+  },
+  {
+    "revision": "1e05899cf47c637a5f48c373e05046fd",
+    "url": "/images/practical-areas/s1.jpg"
+  },
+  {
+    "revision": "235675fafc574512b8b21d2b712a49ac",
+    "url": "/images/practical-areas/s10.jpg"
+  },
+  {
+    "revision": "0f37e1d101489389915f515c2f53af62",
+    "url": "/images/practical-areas/s11.jpg"
+  },
+  {
+    "revision": "716fbac6f0014489eb4559cbc46d92c6",
+    "url": "/images/practical-areas/s3-1.jpg"
+  },
+  {
+    "revision": "8f253d6e224a02e3dc4e107fdfd34391",
+    "url": "/images/practical-areas/s4-1.jpg"
+  },
+  {
+    "revision": "564cc530e1ca710fa4e4949d701e6318",
+    "url": "/images/practical-areas/s4-2.jpg"
+  },
+  {
+    "revision": "b520c2930fe1e420f62da4a33658a4e8",
+    "url": "/images/practical-areas/s4.jpg"
+  },
+  {
+    "revision": "7751cdf358948da83aaa78868dbfba05",
+    "url": "/images/practical-areas/s5-2.jpg"
+  },
+  {
+    "revision": "f722e2e7e97f7390e10cd524ccf60976",
+    "url": "/images/practical-areas/s5-3.jpg"
+  },
+  {
+    "revision": "fa9c0d546815398791207a39eedf4cf3",
+    "url": "/images/practical-areas/s5.jpg"
+  },
+  {
+    "revision": "abe585eaf0385c5f689053ba1da6a869",
+    "url": "/images/practical-areas/s6-1.jpg"
+  },
+  {
+    "revision": "f5b08675669d6b8bd4c3eb27372e241a",
+    "url": "/images/practical-areas/s8.jpg"
+  },
+  {
+    "revision": "03eed4a9bf3882c5edeab8fb0316e13b",
+    "url": "/images/practical-areas/s9.jpg"
+  },
+  {
+    "revision": "41dd21b0e9fc80972c78f0143e1b18a9",
+    "url": "/images/practical-bg.jpg"
+  },
+  {
+    "revision": "83407da6fd356b544eee490d0e53f173",
+    "url": "/images/services.png"
+  },
+  {
+    "revision": "991c57e9424f3c385a274d7d8ca52ae6",
+    "url": "/images/tim-mossholder-H6eaxcGNQbU-unsplash.jpg"
+  },
+  {
+    "revision": "b9328a37e4f5aade43e4d1df08b8d421",
+    "url": "/img/3619-2.b9328a37.jpg"
+  },
+  {
+    "revision": "1946b335f0b1b1b7dc2165890065dd1d",
+    "url": "/img/3656-2.1946b335.jpg"
+  },
+  {
+    "revision": "c3637e4690f98e7a8660cf48045109d8",
+    "url": "/img/about-firm.c3637e46.jpg"
+  },
+  {
+    "revision": "5f4a503afcc43f62302ca7fed09dd7d0",
+    "url": "/img/logo.5f4a503a.svg"
+  },
+  {
+    "revision": "0488a41d68aa36816931cac2be18bec3",
+    "url": "/img/news1.0488a41d.jpg"
+  },
+  {
+    "revision": "dae657978e27fd7a08bbd5f76296688b",
+    "url": "/img/news2.dae65797.jpg"
+  },
+  {
+    "revision": "991c57e9424f3c385a274d7d8ca52ae6",
+    "url": "/img/news3.991c57e9.jpg"
+  },
+  {
+    "revision": "7a4db6df63ae1dbeeebc37d9a35bfbc0",
+    "url": "/img/photo_2020-02-24_10-14-28.7a4db6df.jpg"
+  },
+  {
+    "revision": "1e05899cf47c637a5f48c373e05046fd",
+    "url": "/img/s1.1e05899c.jpg"
+  },
+  {
+    "revision": "235675fafc574512b8b21d2b712a49ac",
+    "url": "/img/s10.235675fa.jpg"
+  },
+  {
+    "revision": "0f37e1d101489389915f515c2f53af62",
+    "url": "/img/s11.0f37e1d1.jpg"
+  },
+  {
+    "revision": "716fbac6f0014489eb4559cbc46d92c6",
+    "url": "/img/s3-1.716fbac6.jpg"
+  },
+  {
+    "revision": "b520c2930fe1e420f62da4a33658a4e8",
+    "url": "/img/s4.b520c293.jpg"
+  },
+  {
+    "revision": "fa9c0d546815398791207a39eedf4cf3",
+    "url": "/img/s5.fa9c0d54.jpg"
+  },
+  {
+    "revision": "abe585eaf0385c5f689053ba1da6a869",
+    "url": "/img/s6-1.abe585ea.jpg"
+  },
+  {
+    "revision": "f5b08675669d6b8bd4c3eb27372e241a",
+    "url": "/img/s8.f5b08675.jpg"
+  },
+  {
+    "revision": "03eed4a9bf3882c5edeab8fb0316e13b",
+    "url": "/img/s9.03eed4a9.jpg"
+  },
+  {
+    "revision": "a97afefcb58a9b82c6a3e71e700964f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e4017f58daf3e82a46d",
+    "url": "/js/app.a138d7e5.js"
+  },
+  {
+    "revision": "8941dc96af802fafdb4d891c24f4cd49",
+    "url": "/js/app.js"
+  },
+  {
+    "revision": "e1d98d47689e00f8ecbc5d9f61bdb42e",
+    "url": "/js/bootstrap.min.js"
+  },
+  {
+    "revision": "af88751412417e8e8e1f",
+    "url": "/js/chunk-vendors.79accf95.js"
+  },
+  {
+    "revision": "99b0a83cf1b0b1e2cb16041520e87641",
+    "url": "/js/jquery-3.3.1.min.js"
+  },
+  {
+    "revision": "ac15a9af0a2e81a91f9b5367b5ba984e",
+    "url": "/js/owl.carousel.js"
+  },
+  {
+    "revision": "369ecfe66fed1374b03e62f27a508730",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "0932325874556fd01694c0e7eca82043",
+    "url": "/profile/profile.pdf"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
